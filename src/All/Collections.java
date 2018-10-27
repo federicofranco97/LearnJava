a04f13e331f1416f6e4546414cae4a3424998e9e
Fede <federicoa.franco@comunidad.ub.edu.ar>
2018-10-27 20:37:46 -0300

    add list

--- src/All/Collections.java
+++ src/All/Collections.java
@@ -1,6 +1,8 @@
 
 package All;
 
+import javax.swing.JOptionPane;
+
 
 /**
  *
@@ -20,22 +22,74 @@ public class Collections extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Collections");
+        setMinimumSize(new java.awt.Dimension(720, 330));
+        getContentPane().setLayout(null);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        jLabel1.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
+        jLabel1.setText("Las formas de almacenar datos que usamos son:");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(76, 39, 574, 41);
+
+        jButton1.setText("JList");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1);
+        jButton1.setBounds(170, 140, 89, 32);
+
+        jButton2.setText("ArrayList");
+        getContentPane().add(jButton2);
+        jButton2.setBounds(300, 140, 89, 32);
+
+        jButton3.setText("HashMap");
+        getContentPane().add(jButton3);
+        jButton3.setBounds(430, 140, 89, 32);
+
+        jButton4.setText("Funciones");
+        getContentPane().add(jButton4);
+        jButton4.setBounds(300, 180, 89, 32);
+
+        jButton5.setText("Funciones");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton5);
+        jButton5.setBounds(170, 180, 89, 32);
+
+        jButton6.setText("Funciones");
+        getContentPane().add(jButton6);
+        jButton6.setBounds(430, 180, 89, 32);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        String msj="Una List es un vector de almacenamiento de datos. Solo almacena\n"
+                + "un tipo de dato, por ejemplo si creamos una lista de Strings solo\n"
+                + "podemos almacenar Strings en esa lista. Es de tamaño FIJO, quiere decir\n"
+                + "que hay que definir el tamaño que tendra y no puede modificarse.\n"
+                + "Se pueden usar para guardar tipos de datos o tambien objetos de una clase\n"
+                + "Existente o creada por el programador";
+        JOptionPane.showMessageDialog(null, msj);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton5ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -76,5 +130,12 @@ public class Collections extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
