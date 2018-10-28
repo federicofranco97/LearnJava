4c42b17425662d22818ce862857657b28b03c670
Fede <federicoa.franco@comunidad.ub.edu.ar>
2018-10-28 15:41:16 -0300

    modifs tuto

--- src/Vids/CrearMetodo.java
+++ src/Vids/CrearMetodo.java
@@ -5,6 +5,8 @@
  */
 package Vids;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Fede
@@ -12,10 +14,11 @@ package Vids;
 public class CrearMetodo extends javax.swing.JFrame {
 
     /**
-     * Creates new form CrearMetodo
+     * Creates new form CrearProyecto
      */
     public CrearMetodo() {
         initComponents();
+        setLocationRelativeTo(null);
     }
 
     /**
@@ -27,21 +30,74 @@ public class CrearMetodo extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(1280, 715));
+        getContentPane().setLayout(null);
+
+        jButton1.setBackground(new java.awt.Color(255, 255, 255));
+        jButton1.setForeground(new java.awt.Color(0, 0, 0));
+        jButton1.setText("Cerrar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1);
+        jButton1.setBounds(1060, 530, 170, 60);
+
+        jButton2.setBackground(new java.awt.Color(255, 255, 255));
+        jButton2.setForeground(new java.awt.Color(0, 0, 0));
+        jButton2.setText("Como Seteo la edad");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton2);
+        jButton2.setBounds(1060, 460, 170, 60);
+
+        jButton3.setBackground(new java.awt.Color(255, 255, 255));
+        jButton3.setForeground(new java.awt.Color(0, 0, 0));
+        jButton3.setText("Como Traigo la edad");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton3);
+        jButton3.setBounds(1060, 390, 170, 60);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Assets/CrearMetodo.gif"))); // NOI18N
+        jLabel1.setMinimumSize(new java.awt.Dimension(1720, 1000));
+        jLabel1.setPreferredSize(new java.awt.Dimension(1720, 1000));
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(0, 0, 1720, 720);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        this.setVisible(false);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        String msj="Para setear la nueva edad usamos el metodo setEdad( ); \n"
+                + " Este metodo lo generamos cuando creamos los getters y setters. ";
+        JOptionPane.showMessageDialog(null, msj);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        String msj="Para traer la edad usamos el metodo getEdad( ); \n"
+                + "Este metodo lo generamos cuando creamos los getters y setters.\n"
+                + "NOTA:podemos guardarla en una variable o directamente sumarle 1:\n"
+                + " (getEdad()+1) ";
+        JOptionPane.showMessageDialog(null, msj);
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -69,6 +125,9 @@ public class CrearMetodo extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(CrearMetodo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -79,5 +138,9 @@ public class CrearMetodo extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
